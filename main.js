406e794c9930124ed5bf7e1d09182785b9f0897b
Edgar Negrón <edgar.negron.rabell@gmail.com>
2019-10-27 18:38:48 -0400

    Add click events for squares to give them functionality

--- main.js
+++ main.js
@@ -1,17 +1,44 @@
+let colors = [
+  `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`, 
+  `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`,
+  `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`,
+  `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`,
+  `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`,
+  `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`
+];
 
+let pickedColor = colors[3]
+const body = document.querySelector("body");
+const squares = document.querySelectorAll(".squares");
+const randomizer = document.querySelector("#randomize");
+const rgbDisplay = document.querySelector("#rgbDisplay");
+const message = document.querySelector("#message");
+rgbDisplay.innerHTML = `${pickedColor}`;
 
+for( let i = 0; i < squares.length; i++ ) {
+  //Initial color of squares
+  squares[i].style.background = colors[i];
+  squares[i].addEventListener("click", function() {
+    let clickedColor = this.style.background;
+    if(pickedColor === clickedColor) {
+      message.innerHTML = `Correct!`
+      body.style.background = `${pickedColor}`;
+    } else {
+      message.innerHTML = `Try again!`
+      this.style.transition = "ease .2s"
+      this.style.visibility = "hidden";
+    }
+  })
+}
+
+randomizer.addEventListener("click", function(){
+  for( let i = 0; i < squares.length; i++ ) {
+    squares[i].style.background = colors[i];
+    rgbDisplay.innerHTML = `${pickedColor}`
+  }
+}); 
 function randomNumber() {
   return Math.floor(Math.random() * 255 );
 }
-let colors = [`rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`, 
-              `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`,
-              `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`,
-              `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`,
-              `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`,
-              `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`];
 
-const squares = document.querySelectorAll(".squares");
 
-for( let i = 0; i < squares.length; i++ ) {
-  squares[i].style.background = colors[i];
-}
